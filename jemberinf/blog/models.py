aba2edf0be4df09ee30697387d9921444ddf675c
AlifvianM <42725407+AlifvianM@users.noreply.github.com>
2019-09-28 00:29:57 +0700

    Add files via upload

---
+++ jemberinf/blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from PIL import Image
+from django.urls import reverse
+# Create your models here.
+
+class Post(models.Model):
+    judul       = models.CharField(max_length = 255)
+    isi         = models.TextField()
+    foto        = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    waktu       = models.DateTimeField(default = timezone.now)
+    penulis     = models.ForeignKey(User, on_delete =  models.CASCADE)
+
+    def __str__(self):
+        return self.judul
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.foto.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.foto.path)
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
---
+++ jemberinf/blog/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Post
+
+# Create your views here.
+
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html'
+	context_object_name = 'posts'
+	paginate_by = 5
+	ordering = ['-waktu']
+
+
+class PostDetailView(DetailView):
+	model = Post
+
+class PostCreateView(LoginRequiredMixin ,CreateView):
+	model = Post
+	fields = [
+		'judul',
+		'isi',
+		'foto',
+	]
+
+	def form_valid(self, form):
+		form.instance.penulis = self.request.user
+		return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin ,UpdateView):
+	model = Post
+	fields = [
+		'judul',
+		'isi',
+		'foto',
+	]
+
+	def form_valid(self, form):
+		form.instance.penulis = self.request.user
+		return super().form_valid(form)
+
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user==post.penulis:
+			return True
+		return False
+
+
+def home(request):
+	posts = Post.objects.all()
+	context = {
+		'posts' : posts
+	}
+	print(Post)
+	return render(request, 'blog/home.html', context)
+
+def about(request):
+	return render(request, 'blog/about.html', {'title':'About'})
